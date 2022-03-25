84308d7bfc62a037c866dcea5374d6789f9f755f
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-03-25 05:58:26 +0300

    Add live url for the app

--- src/ProjectsManager.App/Services/HttpClientHandler.cs
+++ src/ProjectsManager.App/Services/HttpClientHandler.cs
@@ -4,12 +4,15 @@ namespace ProjectsManager.App.Services;
 
 internal class HttpClientHandler
 {
+    private const string localhost = "https://localhost:6001";
+    private const string production = "https://nw9un0fjc2.execute-api.us-east-1.amazonaws.com/Prod";
+
     private readonly HttpClient _client;
 
     public HttpClientHandler()
     {
         _client = new HttpClient();
-        _client.BaseAddress = new Uri("https://localhost:6001/api/projects");
+        _client.BaseAddress = new Uri($"{production}/api/projects");
     }
 
     public async Task<T?> GetAllAsync<T>(string endpoint = "")
